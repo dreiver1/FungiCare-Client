fc896a70bcbb3b6d03638366cbe776a16a04e473
david nascimento <viniciosdavid1@gmail.com>
2023-01-28 13:12:02 -0300

    alterando o layout corigindo bug da barra de notificação e alterando nome do aplicativo no build

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header class="text-white" style="background-color: none;">
-      <q-bar dense class="text-white" style="background-color: primary;">
-        <div>10:00</div>
-        <q-icon name="email" />
-        <q-space />
-        <q-icon name="signal_cellular_4_bar" />
-        <q-icon name="signal_wifi_4_bar" />
-        <div class="gt-xs">100%</div>
-        <q-icon name="battery_full" />
-      </q-bar>
       <q-toolbar>
-
         <q-toolbar-title class="q-ma-lg">
           <q-icon name="arrow_back_ios" size="xl"/>
         </q-toolbar-title>
--- src/layouts/cadastroLayout.vue
+++ src/layouts/cadastroLayout.vue
@@ -1,16 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="text-black bg-white">
-      <q-bar dense class="text-black bg-white" >
-        <div>10:00</div>
-        <q-icon name="email" />
-        <q-space />
-        <q-icon name="signal_cellular_4_bar" />
-        <q-icon name="signal_wifi_4_bar" />
-        <div class="gt-xs">100%</div>
-        <q-icon name="battery_full" />
-      </q-bar>
-    </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -21,7 +10,7 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: 'cadastroLayout',
 
   components: {
   },

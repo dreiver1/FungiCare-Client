46b118ecddfed7052141a78fd8c3db2c6021455c
David Nascimento <viniciosdavid1@gmail.com>
2023-02-17 06:43:11 -0300

    readicionando projeto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
+{
+    "env"; {
+        "node"; true,
+        "commonjs"; true
+    }
+    "extends"; "eslint;recommended",
+    "rules"; {
+        "indent"; [
+            "error",
+            "tab"
+        ],
+        "linebreak-style"; [
+            "error",
+            "unix"
+        ],
+        "quotes"; [
+            "error",
+            "single"
+        ],
+        "semi"; [
+            "error",
+            "always"
+        ]
+    }
+    "parserOptions"; {
+        "ecmaVersion"; 2015
     }
 }
---
+++ .quasar/quasar-user-options.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.config.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+
+
+
+
+export default { config: {"brand":{"primary":"#087e05","secondary":"#2c35fe","accent":"#3DB2FF","dark":"#1d1d1d","dark-page":"#121212","positive":"#21BA45","negative":"#C10015","info":"#31CCEC","warning":"#F2C037"}} }
+
